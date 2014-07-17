45a6b6fee2116db6b95a20589c492abe26c62142
Benjamin Bertrand <benjamin.bertrand@lfv.se>
2014-07-17 09:00:09 +0200

    Set recorded timestamp to milliseconds
    - PCAP timestamp added
    - For compatibility between PCAP and final format,
    milliseconds since midnight are used for timestamp

--- src/asterix/asterixpcapsubformat.cxx
+++ src/asterix/asterixpcapsubformat.cxx
@@ -101,6 +101,9 @@ bool CAsterixPcapSubformat::ReadPacket(CBaseFormatDescriptor &formatDescriptor, 
 		return false;
 	}
 
+	// Save PCAP packet timestamp (keep milliseconds since midnight)
+	unsigned long nTimestamp = (m_ePcapRecHeader.ts_sec % 86400) * 1000 + m_ePcapRecHeader.ts_usec / 1000;
+
 	if (gSynchronous)
 	{ // In synchronous mode make delays between packets to simulate real tempo
 		struct timeval currTime;
@@ -269,7 +272,7 @@ bool CAsterixPcapSubformat::ReadPacket(CBaseFormatDescriptor &formatDescriptor, 
 				break;
 
 			// Parse ASTERIX data
-			AsterixData* m_ptmpAsterixData = Descriptor.m_InputParser.parsePacket(pPacketPtr, byteCount-6);
+			AsterixData* m_ptmpAsterixData = Descriptor.m_InputParser.parsePacket(pPacketPtr, byteCount-6, nTimestamp);
 
 			if (Descriptor.m_pAsterixData == NULL)
 			{
@@ -286,7 +289,7 @@ bool CAsterixPcapSubformat::ReadPacket(CBaseFormatDescriptor &formatDescriptor, 
 	}
 	else
 	{
-		Descriptor.m_pAsterixData = Descriptor.m_InputParser.parsePacket(pPacketPtr, m_nDataLength);
+		Descriptor.m_pAsterixData = Descriptor.m_InputParser.parsePacket(pPacketPtr, m_nDataLength, nTimestamp);
 	}
 
 	delete pPacketBuffer;
