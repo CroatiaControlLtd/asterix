c168791d965ec312f89d004f4853d5ad46745fab
Damir Salantić <damir.salantic@gmail.com>
2016-07-05 08:41:51 +0000

    Memory leak test OK

--- install/test/valgrind_test.sh
+++ install/test/valgrind_test.sh
@@ -1,1 +1,7 @@
-valgrind --leak-check=full --show-leak-kinds=all ../obj/main/debug/asterix -P -d config/asterix.ini -f sample_data/cat_062_065.pcap -jh
+valgrind -v --leak-check=full --show-leak-kinds=all --error-exitcode=1 ../../obj/main/debug/asterix -P -d ../config/asterix.ini -f ../sample_data/cat_062_065.pcap -jh
+
+if [ $? = 1 ]; then
+        echo "Memory leak test failed"
+else
+        echo "Memory leak test OK"
+fi
