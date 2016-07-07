4f09e3b52cb93071b27e47bba0e1e6ae545b784f
Damir Salantić <damir.salantic@crocontrol.hr>
2016-07-07 09:44:47 +0200

    Updated tests

--- install/test/test.sh
+++ install/test/test.sh
@@ -31,13 +31,13 @@ test_output () {
 }
 
 
-test_compare "Test filter" "$exec -d $config -L" "../sample_data/filter.txt"
-test_compare "Test json CAT_062_065" "$exec -d $config -P -j -f ../sample_data/cat_062_065.pcap" "../sample_data/cat_062_065_json.txt"
-test_compare "Test json CAT_001_002" "$exec -d $config -R -j -f ../sample_data/cat_001_002.pcap -LF ../sample_data/filter.txt" "../sample_data/cat_001_002_json.txt"
-test_compare "Test jsonh CAT_001_002" "$exec -d $config -R -jh -f ../sample_data/cat_001_002.pcap -LF ../sample_data/filter.txt" "../sample_data/cat_001_002_jsonh.txt"
-test_compare "Test xml CAT_001_002" "$exec -d $config -R -x -f ../sample_data/cat_001_002.pcap -LF ../sample_data/filter.txt" "../sample_data/cat_001_002.xml"
-test_compare "Test txt CAT_001_002" "$exec -d $config -R -f ../sample_data/cat_001_002.pcap -LF ../sample_data/filter.txt" "../sample_data/cat_001_002.txt"
-test_compare "Test line CAT_001_002" "$exec -d $config -R -l -f ../sample_data/cat_001_002.pcap -LF ../sample_data/filter.txt" "../sample_data/cat_001_002_line.txt"
+test_compare "Test filter" "$exec -d $config -L" "../sample_output/filter.txt"
+test_compare "Test json CAT_062_065" "$exec -d $config -P -j -f ../sample_data/cat_062_065.pcap" "../sample_output/cat_062_065_json.txt"
+test_compare "Test json CAT_001_002" "$exec -d $config -R -j -f ../sample_data/cat_001_002.pcap -LF ../sample_output/filter.txt" "../sample_output/cat_001_002_json.txt"
+test_compare "Test jsonh CAT_001_002" "$exec -d $config -R -jh -f ../sample_data/cat_001_002.pcap -LF ../sample_output/filter.txt" "../sample_output/cat_001_002_jsonh.txt"
+test_compare "Test xml CAT_001_002" "$exec -d $config -R -x -f ../sample_data/cat_001_002.pcap -LF ../sample_output/filter.txt" "../sample_output/cat_001_002.xml"
+test_compare "Test txt CAT_001_002" "$exec -d $config -R -f ../sample_data/cat_001_002.pcap -LF ../sample_output/filter.txt" "../sample_output/cat_001_002.txt"
+test_compare "Test line CAT_001_002" "$exec -d $config -R -l -f ../sample_data/cat_001_002.pcap -LF ../sample_output/filter.txt" "../sample_output/cat_001_002_line.txt"
 #test_output  "Test Memory leak with valgrind" "valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=1 $execd -P -d $config -f ../sample_data/cat_062_065.pcap -jh" "0"
 test_output  "Test Memory leak with valgrind" "valgrind --leak-check=full --error-exitcode=1 $execd -P -d $config -f ../sample_data/cat_062_065.pcap -jh" "0"
 
--- install/test/updatetests.sh
+++ install/test/updatetests.sh
@@ -5,12 +5,12 @@ config="../config/asterix.ini"
 
 echo Updating tests
 
-$exec -d $config -L > "../sample_data/filter.txt"
-$exec -d $config -P -j -f ../sample_data/cat_062_065.pcap > "../sample_data/cat_062_065_json.txt"
-$exec -d $config -R -j -f ../sample_data/cat_001_002.pcap -FL ../sample_data/filter.txt > "../sample_data/cat_001_002_json.txt"
-$exec -d $config -R -jh -f ../sample_data/cat_001_002.pcap -FL ../sample_data/filter.txt > "../sample_data/cat_001_002_jsonh.txt"
-$exec -d $config -R -x -f ../sample_data/cat_001_002.pcap -FL ../sample_data/filter.txt > "../sample_data/cat_001_002.xml"
-$exec -d $config -R -f ../sample_data/cat_001_002.pcap -FL ../sample_data/filter.txt > "../sample_data/cat_001_002.txt"
-$exec -d $config -R -l -f ../sample_data/cat_001_002.pcap -FL ../sample_data/filter.txt > "../sample_data/cat_001_002_line.txt"
+$exec -d $config -L > "../sample_output/filter.txt"
+$exec -d $config -P -j -f ../sample_data/cat_062_065.pcap > "../sample_output/cat_062_065_json.txt"
+$exec -d $config -R -j -f ../sample_data/cat_001_002.pcap -FL ../sample_output/filter.txt > "../sample_output/cat_001_002_json.txt"
+$exec -d $config -R -jh -f ../sample_data/cat_001_002.pcap -FL ../sample_output/filter.txt > "../sample_output/cat_001_002_jsonh.txt"
+$exec -d $config -R -x -f ../sample_data/cat_001_002.pcap -FL ../sample_output/filter.txt > "../sample_output/cat_001_002.xml"
+$exec -d $config -R -f ../sample_data/cat_001_002.pcap -FL ../sample_output/filter.txt > "../sample_output/cat_001_002.txt"
+$exec -d $config -R -l -f ../sample_data/cat_001_002.pcap -FL ../sample_output/filter.txt > "../sample_output/cat_001_002_line.txt"
 
 echo Done
