ff0ad3a3360a141d8d907618971bab705bff23be
dsalantic <damir.salantic@crocontrol.hr>
2016-07-25 05:54:42 -0700

    setup.py updated

--- setup.py
+++ setup.py
@@ -16,8 +16,12 @@ CLASSIFIERS = [
 'License :: OSI Approved :: GNU General Public License (GPL)',
 'Programming Language :: C',
 'Programming Language :: C++',
+'Programming Language :: Python :: 2.6',
 'Programming Language :: Python :: 2.7',
+'Programming Language :: Python :: 3.2',
+'Programming Language :: Python :: 3.3',
 'Programming Language :: Python :: 3.4',
+'Programming Language :: Python :: 3.5'
 ]
 
 try:
