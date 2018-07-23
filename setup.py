02195c5d4f66f76e91d8ee4efffad5e84eb9cd80
dsalantic <damir.salantic@gmail.com>
2018-07-23 21:57:16 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ CLASSIFIERS = [
 'Programming Language :: Python :: 3.2',
 'Programming Language :: Python :: 3.3',
 'Programming Language :: Python :: 3.4',
-'Programming Language :: Python :: 3.5'
+'Programming Language :: Python :: 3.5',
 'Programming Language :: Python :: 3.6'
 ]
 
